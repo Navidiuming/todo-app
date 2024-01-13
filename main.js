79038a25a366f2d90dfb00beea643bf3bdd56f86
Navidiuming <119816437+Navidiuming@users.noreply.github.com>
2024-01-13 23:30:12 +0330

    Add files via upload
    add local storage

--- main.js
+++ main.js
@@ -1,21 +1,65 @@
 const btn_addBtn = document.getElementById('addBtn');
 const input_userNewTask = document.getElementById('userNewTask');
 const ul_addedTask = document.querySelector('.addedTask');
-console.log(ul_addedTask);
+main();
+function main() {
+    makeTodoElement(JSON.parse(localStorage.getItem("todos")));
+    btn_addBtn.addEventListener('click', () => {
+        const newTask = input_userNewTask.value.trim();
+        if (newTask) {
+            input_userNewTask.value = "";
+            const todos = !localStorage.getItem("todos") ? []
+                : JSON.parse(localStorage.getItem("todos"));
 
-btn_addBtn.addEventListener('click',()=>{
-    console.log(input_userNewTask.value);
-    if(input_userNewTask.value != ""){
-        let newTask = input_userNewTask.value;
+            console.log(todos);
+            const currentTodo = {
+                name: newTask,
+                isCompeleted: false
+            }
+
+            todos.push(currentTodo);
+            localStorage.setItem("todos", JSON.stringify(todos));
+            makeTodoElement(todos);
+        }
+    })
+
+    input_userNewTask.addEventListener('keydown', (e)=>{
+        if(e.key == "Enter"){
+            console.log('enter pressed')
+            btn_addBtn.click();
+        }
+    })
+}
+
+
+function removeTask(el) {
+    console.log(el);
+    const currentTask = el.parentElement;
+    const currentTaskIndex = [...document.querySelectorAll("li.item")].indexOf(currentTask);
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    // نکته جالب اینکه چه من در عبارت پایین  currentTask  رو بزارم چه currentTaskIndex  رو کار میکنه
+    todos.splice(currentTaskIndex,1);
+    localStorage.setItem("todos" , JSON.stringify(todos));
+    el.parentElement.remove();
+}
+
+
+function makeTodoElement(todoArry) {
+    if (!todoArry) {
+        return null;
+    }
+    ul_addedTask.innerHTML = "";
+    todoArry.forEach(todosObject => {
+        let task = todosObject.name;
         let input_checkBox = document.createElement("input");
-        input_checkBox.setAttribute("type","checkbox");
+        input_checkBox.setAttribute("type", "checkbox");
         let p = document.createElement('p');
-        p.innerHTML = newTask;
+        p.innerHTML = task;
         let button = document.createElement('button');
         let img = document.createElement('img');
         img.src = "./assets/images/icon-cross.svg";
         button.appendChild(img);
-        button.addEventListener('click',()=>{
+        button.addEventListener('click', () => {
             removeTask(button);
         });
         let li = document.createElement('li');
@@ -24,14 +68,13 @@ btn_addBtn.addEventListener('click',()=>{
         li.appendChild(p);
         li.appendChild(button);
         ul_addedTask.appendChild(li);
-        input_userNewTask.value = '';
+        input_checkBox.addEventListener('change' , ()=>{
+            // e.parentElement.classList.toggle('completed');
+            console.log(input_checkBox.parentElement);
+            input_checkBox.parentElement.classList.toggle('completed');
+        })
 
-    }
-})
-
-function removeTask(el){
-    console.log(el);
-    el.parentElement.remove();
+    })
 }
 /*      <li class="item">
             <input type="checkbox">
